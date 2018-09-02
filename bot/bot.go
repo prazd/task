f92a7a900baf054a4a239f906a62fb3655d88227
prazd <alexokda@list.ru>
2018-09-02 18:42:16 +0300

    fix

--- bot/bot.go
+++ bot/bot.go
@@ -701,7 +701,7 @@ func main() {
 						log.Println(err)
 					}
 
-					ps := portscanner.NewPortScanner("localhost", 5*time1.Second, 5)
+					ps := portscanner.NewPortScanner("localhost", 4*time.Second, 5)
 					serverPort := ps.IsOpen(3000)
 
 					if serverPort == false {
